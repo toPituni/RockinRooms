8c6df57d6ff0f64f19a199630aac6162806c0178
teena ajith <teena.ajith@gmail.com>
2020-02-26 15:07:17 +0100

    search working

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,29 +7,31 @@ class RoomsController < ApplicationController
 
       @rooms = Room.all.where("district LIKE :search", search: district)
 
+      # Replace this code with correct ActiveRecord queries
       if @rooms.empty?
         flash[:notice] = "No rooms in #{district}!."
       else
         available_rooms= []
         @rooms.each do |room|
-        @bookings = Booking.all.where(room: room)
-
-        @bookings.each do |booking|
+          @bookings = room.bookings
+          @bookings.each do |booking|
             if booking.start_time != start_date && booking.start_time != end_date
-              available_rooms << Room.find(booking.room_id)
+              available_rooms << booking.room
             else
               flash[:notice] = "No rooms in on these days!."
-              break
-            end
             break
-          end
+            end
           break
+          end
+        break
         end
-        @rooms = available_rooms
+        @rooms = available_rooms.uniq
       end
+
     else
-      @rooms = Room.all
+        @rooms = Room.all
     end
+
   end
 
   def create
