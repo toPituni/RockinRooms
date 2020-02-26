2b2abc93fc09e86bf6f32913e7a90a8e2bd0284d
teena ajith <teena.ajith@gmail.com>
2020-02-26 16:32:23 +0100

    Search function working properly

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,42 +1,32 @@
 class RoomsController < ApplicationController
   def index
-    if params[:search].present?
-      district =  params["search"]["district"]
-      start_date = params["search"]["start_date"]
-      end_date = params["search"]["end_date"]
+    @rooms = Room.all
+    @rooms = filter_district(@rooms) if params[:search][:district].present?
 
-      @rooms = Room.all.where("district LIKE :search", search: district)
+    dates_present = params[:search][:start_date].present? &&
+                    params[:search][:end_date].present?
 
-      # Replace this code with correct ActiveRecord queries
-      if @rooms.empty?
-        flash[:notice] = "No rooms in #{district}!."
-      else
-        available_rooms= []
-        @rooms.each do |room|
-          @bookings = room.bookings
-          @bookings.each do |booking|
-            if booking.start_time != start_date && booking.start_time != end_date
-              available_rooms << booking.room
-            else
-              flash[:notice] = "No rooms in on these days!."
-            break
-            end
-          break
-          end
-        break
-        end
-        @rooms = available_rooms.uniq
-      end
-
-    else
-        @rooms = Room.all
-    end
-
+    @rooms = filter_dates(@rooms) if dates_present
   end
 
   def create
   end
 
   def new
+  end
+
+  private
+
+  def filter_district(scope)
+    scope.where(district: params[:search][:district])
+  end
+
+  def filter_dates(scope)
+    start_time = params[:search][:start_date]
+    end_time = params[:search][:end_date]
+    search_range = start_time.to_time.utc..end_time.to_time.utc
+    scope.joins(:bookings)
+         .where.not(bookings: { start_time: search_range } )
+         .uniq
   end
 end
