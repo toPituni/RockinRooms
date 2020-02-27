d3f0975732b18a79cfb43aa17ecff6c8b7e23003
Janis Vaneylen <janis.vaneylen@gmail.com>
2020-02-27 16:17:57 +0100

    dashboard implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   root to: 'pages#home'
   devise_for :users
   resources :rooms do
     resources :bookings, only: [ :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+  resources :dashboard, only: :index
 end
